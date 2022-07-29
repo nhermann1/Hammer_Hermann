eca723f67eb50b369bca2e70d4b0e5c86caf0f69
Nathan Hermann <nh1087@usnh.edu>
2022-07-29 14:59:37 -0400

    Nate: NMDS figures for presenting

--- HammerHermann_Arctic_Foraging_manu.R
+++ HammerHermann_Arctic_Foraging_manu.R
@@ -355,7 +355,6 @@ table(filter(summaries,!is.na(Diet_Mass_g)&!is.na(Mass_g))$Species,
 # Actual data ---------------------------------------------------
 set.seed(42)
 
-
 library(tidyverse)
 library(ggplot2)
 library(reshape2)
@@ -457,7 +456,7 @@ library(indicspecies)
 
 #Duplicating with ALL the counts
 allCount.Mat<-as.matrix(dplyr::select(dietMat,matches("^prey")))
-allEnv.Mat<-as.matrix(dplyr::select(dietMat,-matches("^prey")))
+allEnv.Mat<-(dplyr::select(dietMat,-matches("^prey")))
 
 #Percent zeroes--number of zeroes times dim of matrix
 (sum(allCount.Mat==0)/(nrow(allCount.Mat)*ncol(allCount.Mat)))*100
@@ -496,108 +495,269 @@ stressplot(count_NMDS) #This is the visual of stress, the divergence of observed
 
 
 
+
+# Printing NMDS -----------------------------------------------------------
+
 #Print the species scores and sample scores
 NMDS_species<-as.data.frame(count_NMDS$species)
 NMDS_scores<-as.data.frame(count_NMDS$points)
 
-ggplot()+
-  geom_point(data=NMDS_scores,aes(MDS1,MDS2))+
+speciesPal<-scales::viridis_pal(option="G",begin=0.05,end=0.95)(2)
+yearPal<-scales::viridis_pal(option="H",begin=0.05,end=0.95)(3)
+
+allEnv.Mat<-allEnv.Mat%>%
+  mutate(family=ifelse(Species=="Arctic char","Arctic char","Sculpin"),
+         famCol=ifelse(family=="Arctic char",speciesPal[1],speciesPal[2]),
+         yearCol=case_when(Year==2017~yearPal[1],
+                           Year==2018~yearPal[2],
+                           Year==2019~yearPal[3]))
+
+
+gg_ordiplot <- function(ord, groups, scaling = 1, choices = c(1,2), kind = c("sd", "se", "ehull"), conf=NULL, show.groups="all", ellipse = TRUE, label = FALSE, hull = FALSE, spiders = FALSE, pt.size = 3, plot=TRUE) {
+  groups <- as.factor(groups)
+  if (show.groups[1]=="all") {
+    show.groups <- as.vector(levels(groups))
+  }
+  
+  # Get site coordinates to plot.
+  df_ord <- vegan::scores(ord, display = "sites", scaling=scaling, choices=choices)
+  axis.labels <- ord_labels(ord)[choices]
+  df_ord <- data.frame(x=df_ord[ , 1], y=df_ord[ , 2], Group=groups)
+  
+  # Get ellipse centers to annotate.
+  df_mean.ord <- aggregate(df_ord[,1:2], by=list(df_ord$Group),mean)
+  colnames(df_mean.ord) <- c("Group", "x", "y")
+  df_mean.ord <- df_mean.ord[df_mean.ord$Group %in% show.groups, ]
+  
+  # Get parameters from the ordiellipse function.
+  if (is.null(conf)) {
+    rslt <- vegan::ordiellipse(ord, groups=groups, display = "sites", scaling=scaling, choices=choices, kind = kind, show.groups = show.groups, draw = "none", label = label)
+  } else {
+    rslt <- vegan::ordiellipse(ord, groups=groups, display = "sites", scaling=scaling, choices=choices, kind = kind, show.groups = show.groups, draw = "none", conf = conf, label = label)
+  }
+  
+  # Get points to plot for the ellipses.
+  df_ellipse <- data.frame()
+  for(g in show.groups) {
+    df_ellipse <- rbind(df_ellipse, cbind(as.data.frame(with(df_ord[df_ord$Group==g,],
+                                                             vegan:::veganCovEllipse(rslt[[g]]$cov,rslt[[g]]$center, rslt[[g]]$scale))),Group=g))
+  }
+  colnames(df_ellipse) <- c("x", "y", "Group")
+  df_ellipse <- df_ellipse[ , c(3,1,2)]
+  
+  # Make data frame for hulls.
+  rslt.hull <- vegan::ordihull(ord, groups = groups, scaling = scaling, choices = choices, show.groups = show.groups, draw = "none")
+  df_hull <- data.frame()
+  df_temp <- data.frame()
+  for (g in show.groups) {
+    x <- rslt.hull[[g]][ , 1]
+    y <- rslt.hull[[g]][ , 2]
+    Group <- rep(g, length(x))
+    df_temp <- data.frame(Group = Group, x=x, y=y)
+    df_hull <- rbind(df_hull, df_temp)
+  }
+  
+  # Make a data frame for the spiders.
+  df_spiders <- df_ord
+  df_spiders$cntr.x <- NA
+  df_spiders$cntr.y <- NA
+  for (g in show.groups) {
+    df_spiders[which(df_spiders$Group==g), 4:5] <- df_mean.ord[which(df_mean.ord==g), 2:3]
+  }
+  df_spiders <- df_spiders[ , c(3,4,5,1,2)]
+  df_spiders <- df_spiders[order(df_spiders$Group), ]
+  df_spiders <- df_spiders[df_spiders$Group %in% show.groups, ]
+  
+  # Make basic ggplot with ellipses.
+  xlab <- axis.labels[1]
+  ylab <- axis.labels[2]
+  plt <- ggplot2::ggplot() +
+    geom_point(data=df_ord, aes(x=x, y=y, color=Group), size = pt.size) +
+    xlab(xlab) + ylab(ylab)
+  
+  # Add ellipses.
+  if (ellipse == TRUE) {
+    plt <- plt + geom_path(data = df_ellipse, aes(x=x, y=y, color=Group), show.legend = FALSE)
+  }
+  
+  # Add labels.
+  if (label == TRUE) {
+    plt <- plt + geom_text(data=df_mean.ord, aes(x=x, y=y, label=Group, color=Group), show.legend = FALSE)
+  }
+  
+  # Add hulls.
+  if (hull == TRUE) {
+    plt <- plt + geom_path(data=df_hull, aes(x=x, y=y, color=Group), show.legend = FALSE)
+  }
+  
+  # Add spiders.
+  if (spiders == TRUE) {
+    plt <- plt + geom_segment(data=df_spiders, aes(x=cntr.x, xend=x, y=cntr.y, yend=y, color=Group), show.legend = FALSE)
+  }
+  
+  plt <- plt + coord_fixed(ratio=1)
+  
+  # Plot?
+  if (plot) {print(plt)}
+  
+  # Return data frames, plot as a list.
+  invisible(list(df_ord=df_ord, df_mean.ord=df_mean.ord, df_ellipse=df_ellipse, df_hull=df_hull, df_spiders=df_spiders, plot=plt))
+}
+
+
+speciesCentroids <- aggregate(NMDS_scores, by=list(allEnv.Mat$family),mean)
+yearCentoids <- aggregate(NMDS_scores, by=list(allEnv.Mat$Year),mean)
+spyrCentoids <- aggregate(NMDS_scores, by=list(allEnv.Mat$family,allEnv.Mat$Year),mean)%>%
+  mutate(lab=paste0(ifelse(Group.1=="Sculpin","S","AC"),substr(Group.2,3,4)))
+
+
+axes12<-ggplot()+
+  geom_polygon(data=NMDS_scores%>%group_by(family=allEnv.Mat$family,Year=as.character(allEnv.Mat$Year))%>%slice(chull(MDS1,MDS2)),
+               aes(x=MDS1,y=MDS2,group=paste(family,Year),color=family,lty=Year),alpha=0.1,lwd=1.5)+
+  geom_point(data=NMDS_scores,aes(MDS1,MDS2,fill=allEnv.Mat$family,shape=as.character(allEnv.Mat$Year)),color="black",size=6)+
   geom_segment(data=NMDS_species,aes(x=0,xend=MDS1,y=0,yend=MDS2))+
-  geom_text(data=NMDS_species,aes(x=MDS1,y=MDS2,label=row.names(NMDS_species)))
+  geom_text(data=NMDS_species,aes(x=MDS1,y=MDS2,label=gsub("prey_","",row.names(NMDS_species))))+
+  scale_fill_viridis_d(option="E",name="Family")+
+  scale_color_viridis_d(option="E",name="Family",guide="none")+
+  scale_shape_manual(values=c(21,22,23),name="Year")+
+  scale_linetype_manual(values=c(1,5,3))+
+  guides(fill=guide_legend(override.aes = list(shape=21,size=20)),
+         linetype=guide_legend(override.aes=list(color="black",size=20,fill="transparent")))+
+  #geom_text(aes(x=-1.6,y=-1.6,label=paste0("Stress: ",round(actualStress*100,2))),size=7,hjust=0)+
+  geom_point(data=spyrCentoids,aes(MDS1,MDS2,fill=Group.1,shape=as.character(Group.2)),
+             color="red",size=12,stroke=2,alpha=0.7,show.legend=F)+
+  geom_label(data=spyrCentoids,aes(MDS1,MDS2,label=lab),
+             color="red",size=3,alpha=0.7,show.legend=F)+
+  xlab("Axis 1")+ylab("Axis 2")
 
-ggplot()+
-  geom_point(data=NMDS_scores,aes(MDS1,MDS3))+
+
+axes13<-ggplot()+
+  geom_polygon(data=NMDS_scores%>%group_by(family=allEnv.Mat$family,Year=as.character(allEnv.Mat$Year))%>%slice(chull(MDS1,MDS3)),
+               aes(x=MDS1,y=MDS3,group=paste(family,Year),color=family,lty=Year),alpha=0.1,lwd=1.5)+
+  geom_point(data=NMDS_scores,aes(MDS1,MDS3,fill=allEnv.Mat$family,shape=as.character(allEnv.Mat$Year)),color="black",size=6)+
   geom_segment(data=NMDS_species,aes(x=0,xend=MDS1,y=0,yend=MDS3))+
-  geom_text(data=NMDS_species,aes(x=MDS1,y=MDS3,label=row.names(NMDS_species)))
+  geom_text(data=NMDS_species,aes(x=MDS1,y=MDS3,label=gsub("prey_","",row.names(NMDS_species))))+
+  scale_fill_viridis_d(option="E",name="Family")+
+  scale_color_viridis_d(option="E",name="Family",guide="none")+
+  scale_shape_manual(values=c(21,22,23),name="Year")+
+  scale_linetype_manual(values=c(1,5,3))+
+  guides(fill=guide_legend(override.aes = list(shape=21,size=20)),
+         linetype=guide_legend(override.aes=list(color="black",size=20,fill="transparent")))+
+  #geom_text(aes(x=-1.6,y=-1.6,label=paste0("Stress: ",round(actualStress*100,2))),size=6,hjust=0)+
+  geom_point(data=spyrCentoids,aes(MDS1,MDS3,fill=Group.1,shape=as.character(Group.2)),
+             color="red",size=12,stroke=2,alpha=0.7,show.legend=F)+
+  geom_label(data=spyrCentoids,aes(MDS1,MDS3,label=lab),
+             color="red",size=3,alpha=0.7,show.legend=F)+
+  xlab("Axis 1")+ylab("Axis 3")
 
-ggplot()+
-  geom_point(data=NMDS_scores,aes(MDS2,MDS3))+
+axes23<-ggplot()+
+  geom_polygon(data=NMDS_scores%>%group_by(family=allEnv.Mat$family,Year=as.character(allEnv.Mat$Year))%>%slice(chull(MDS2,MDS3)),
+               aes(x=MDS2,y=MDS3,group=paste(family,Year),color=family,lty=Year),alpha=0.1,lwd=1.5)+
+  geom_point(data=NMDS_scores,aes(MDS2,MDS3,fill=allEnv.Mat$family,shape=as.character(allEnv.Mat$Year)),color="black",size=6)+
   geom_segment(data=NMDS_species,aes(x=0,xend=MDS2,y=0,yend=MDS3))+
-  geom_text(data=NMDS_species,aes(x=MDS2,y=MDS3,label=row.names(NMDS_species)))
+  geom_text(data=NMDS_species,aes(x=MDS2,y=MDS3,label=gsub("prey_","",row.names(NMDS_species))))+
+  scale_fill_viridis_d(option="E",name="Family")+
+  scale_color_viridis_d(option="E",name="Family",guide="none")+
+  scale_shape_manual(values=c(21,22,23),name="Year")+
+  scale_linetype_manual(values=c(1,5,3))+
+  scale_y_continuous(breaks=c(-1,0,1))+
+  guides(fill=guide_legend(override.aes = list(shape=21,size=20)),
+         linetype=guide_legend(override.aes=list(color="black",size=20,fill="transparent")))+
+  geom_text(aes(x=-1.9,y=-1.2,label=paste0("Stress: ",round(actualStress*100,2))),size=6,hjust=0)+
+  geom_point(data=spyrCentoids,aes(MDS2,MDS3,fill=Group.1,shape=as.character(Group.2)),
+             color="red",size=12,stroke=2,alpha=0.7,show.legend=F)+
+  geom_label(data=spyrCentoids,aes(MDS2,MDS3,label=lab),
+             color="red",size=3,alpha=0.7,show.legend=F)+
+  xlab("Axis 2")+ylab("Axis 3")
 
-library(plot3D)
+library(ggpubr)
+ggarrange(axes12,axes13,axes23,common.legend = T,legend="top",ncol=1)
 
-points3D(NMDS_scores$MDS1,NMDS_scores$MDS2,NMDS_scores$MDS3,phi=30,theta=45,col="red")
-segments3D(x0=rep(0,nrow(NMDS_species)),y0=rep(0,nrow(NMDS_species)),z0=rep(0,nrow(NMDS_species)),
-             x1=NMDS_species$MDS1,y1=NMDS_species$MDS2,z1=NMDS_species$MDS3,add=T)
+
+library(rgl)
+interleave <- function(v1, v2) as.vector(rbind(v1,v2))
+
+plot3d(NMDS_scores$MDS1,NMDS_scores$MDS2,NMDS_scores$MDS3,
+       size=8,col=allEnv.Mat$famCol)
+segments3d(interleave(0, NMDS_species$MDS1),
+           interleave(0, NMDS_species$MDS2),
+           interleave(0, NMDS_species$MDS3),
+           alpha = 0.4, col = "blue")
+text3d(NMDS_species$MDS1,
+       NMDS_species$MDS2,
+       NMDS_species$MDS3,
+       texts=gsub("prey_","",row.names(NMDS_species)),add=T)
+play3d(spin3d())
 
 
 
 
 # Ignore for now ----------------------------------------------------------
 
-
-
-#Creating the environmental character groupings in a df
-allEnv.DF<-as.data.frame(allEnv.Mat)%>%
-  mutate(Species=ifelse(grepl("horn",Species),"Fourhorn","Slimy"),
-         TL_Fish=as.numeric(as.character(TL_Fish)),
-         Mass_Fish=as.numeric(as.character(Mass_Fish)),
-         Size_Class=ifelse(Mass_Fish>=100,"Large", #28 individuals
-                           ifelse(Mass_Fish>=59.5 & Mass_Fish<100,"Medium", #28 Individuals
-                                  ifelse(Mass_Fish<34,"Extra-Small","Small"))), #26 and 28 individuals, respectively
-         Size_Class=ifelse(is.na(Size_Class),"Medium",Size_Class),
-         Size_Class=factor(Size_Class,levels=c("Extra-Small","Small","Medium","Large")),
-         fortnight=ifelse(as.numeric(yday(Date))<min(as.numeric(yday(Date)))+14,"Early",
-                          ifelse(as.numeric(yday(Date))>min(as.numeric(yday(Date)))+13 & 
-                                   as.numeric(yday(Date))<min(as.numeric(yday(Date)))+28,"Middle","Late")),
-         fortName=ifelse(fortnight=="Early","July 23-Aug 5",
-                         ifelse(fortnight=="Middle","Aug 6-Aug 19","Aug 20-Sept 6")),
-         fortName=factor(fortName,levels=c("July 23-Aug 5","Aug 6-Aug 19","Aug 20-Sept 6")),
-         Sex=ifelse(Sex%notin%c("M","F"),"Unk",Sex),
-         Year=as.character(year(Date)))
-#Not a very even split on the years and fortnights (only 1 2018 early sample)
-
-
 #PERMANOVA for the interaction of Year and fortnight 
 set.seed(42)
-adonis(original.dist~fortName+Year+Size_Class+Species+Sex+fortName:Year+Species:Year+Size_Class:Year:fortName,
-       data=allEnv.DF,permutations=10000,method="bray")
+adonis(original.dist~as.character(Year)*family,
+       data=allEnv.Mat,permutations=1000,method="bray")
 
 #significant pairwise test of the time period
-pairwise.perm.manova(original.dist,allEnv.DF$fortnight,nperm=1000)
+pairwise.perm.manova(original.dist,allEnv.Mat$Year,nperm=1000)
 
-fdisp<-betadisper(original.dist,allEnv.DF$fortnight)
+fdisp<-betadisper(original.dist,allEnv.Mat$Year)
 fdisp
 permutest(fdisp)
 
 library(usedist)
-dist_multi_centroids(original.dist,allEnv.DF$fortnight)
+dist_multi_centroids(original.dist,allEnv.Mat$Year)
 
-#Similar for year
-pairwise.perm.manova(original.dist,allEnv.DF$fortnight,nperm=1000)
 
-ydisp<-betadisper(original.dist,allEnv.DF$Year)
+ydisp<-betadisper(original.dist,allEnv.Mat$family)
 ydisp
 permutest(ydisp)
-dist_multi_centroids(original.dist,allEnv.DF$Year)
-
-#Not sure why this is here
-detections%>%group_by(Fish_ID)%>%summarise(mean=mean(n_distinct(Station.name)))%>%
-  ungroup()%>%summarise(se=sd(mean)/sqrt(79),mean=mean(mean))
-
-#Investigating the same in the size_class and threeway interaction
-pairwise.perm.manova(original.dist,allEnv.DF$Size_Class,nperm=1000)
-pairwise.perm.manova(original.dist,paste(allEnv.DF$fortnight,allEnv.DF$Year,allEnv.DF$Size_Class),nperm=1000)
-table(allEnv.DF$Year,allEnv.DF$Size_Class,allEnv.DF$fortName)
+dist_multi_centroids(original.dist,allEnv.Mat$family)
 
 
-ggplot(allEnv.DF)+
-  geom_bar(aes(fortName,fill=Size_Class),color="black",width=1)+
-  facet_wrap(~Year)+
-  scale_fill_viridis_d(begin=0.2,name="Size Class")+
-  scale_x_discrete(name="Time Period")+
-  scale_y_continuous(expand=expansion(add=c(0.5,4)),name="Frequency")+
-  theme(legend.position = c(0.8,0.8),legend.background = element_rect(color="black",size=1))
 
+sydisp<-betadisper(original.dist,paste(allEnv.Mat$family,allEnv.Mat$Year))
+sydisp
+permutest(ydisp)
+centroidDists<-dist_multi_centroids(original.dist,paste(allEnv.Mat$family,allEnv.Mat$Year))
+labels(centroidDists)
 
+charCentroids<-c(0.5168236,0.6386732,0.4974933)
+sculpCentroids<-c(0.4926337,0.2285743,0.3625625)
+sameYearCentroids<-c(0.2207148,0.3005605,0.5710684)
+interSpeciesCentroids<-c(sameYearCentroids,0.4323695,0.6286381,0.5417022,0.6648193,0.3000332,0.3775107)
+mean(charCentroids)
+mean(sculpCentroids)
+mean(sameYearCentroids)
+mean(interSpeciesCentroids)
+
+centroidDists%>%
+  as.matrix()%>%as.data.frame()%>%
+  mutate(species1=labels(centroidDists))%>%
+  pivot_longer(cols=labels(centroidDists),names_to = "species2", values_to = "dist")%>%
+  ggplot()+
+  geom_tile(aes(species1,species2,fill=dist))+
+  geom_text(aes(species1,species2,label=round(dist,digits=2)),size=4)+
+  geom_rect(aes(xmin=0.5,ymin=0.5,xmax=3.5,ymax=3.5),fill="transparent",color="black",size=2)+
+  geom_rect(aes(xmin=3.5,ymin=0.5,xmax=6.5,ymax=3.5),fill="transparent",color="black",size=2)+
+  geom_rect(aes(xmin=3.5,ymin=3.5,xmax=6.5,ymax=6.5),fill="transparent",color="black",size=2)+
+  geom_rect(aes(xmin=3.5,ymin=0.5,xmax=4.5,ymax=1.5),fill="transparent",color="red",size=2)+
+  geom_rect(aes(xmin=4.5,ymin=1.5,xmax=5.5,ymax=2.5),fill="transparent",color="red",size=2)+
+  geom_rect(aes(xmin=5.5,ymin=2.5,xmax=6.5,ymax=3.5),fill="transparent",color="red",size=2)+
+  scale_fill_viridis_c(option="B",name="Centroid\ndistance",na.value="white")+
+  scale_x_discrete(name="",expand=expansion(0))+
+  scale_y_discrete(name="",expand=expansion(0))+
+  theme(panel.border = element_blank(),axis.text.x=element_text(angle=90,vjust=0.5,hjust=1))
+#Note that the centroids between char are further apart on average (0.55) than the sculpin are (0.36) 
+  #or the two species are from each other in the same year (0.36) or even generally (0.44)
 
 
 library(indicspecies)
 
 
 #ISA to see what diet items might be associated with the different years that make them different from each other
-countYear_ISA = multipatt(as.data.frame(allCount.Mat), allEnv.DF$Year,
+countYear_ISA = multipatt(as.data.frame(allCount.Mat), allEnv.Mat$Year,
                           func = "IndVal.g", duleg=TRUE, control = how(nperm=4999))
 
 #What species?
@@ -610,22 +770,20 @@ yIndSp$Species<-rownames(yIndSp)
 yIndSp$time<-colnames(countYear_ISA$B)[yIndSp$index]
 
 
-
-
 #ISA to see what diet items might be associated with the different time periods
-countFort_ISA = multipatt(as.data.frame(allCount.Mat), allEnv.DF$fortName,
+countSP_ISA = multipatt(as.data.frame(allCount.Mat), allEnv.Mat$family,
                           func = "IndVal.g", duleg=TRUE, control = how(nperm=4999))
 
 #What species?
-summary(countFort_ISA) #Early has Amphipod and Gammaracanthus, Middle has Mysid, Late has Onisimus and Fish
-countFort_ISA$str
+summary(countSP_ISA) #Early has Amphipod and Gammaracanthus, Middle has Mysid, Late has Onisimus and Fish
+countSP_ISA$str
 
 #Extract them, with the year they're significant for
-fIndSp<-dplyr::select(subset(countFort_ISA$sign, p.value<0.05),index,stat,p.value)
-fIndSp$Species<-rownames(fIndSp)
-fIndSp$time<-colnames(countFort_ISA$B)[fIndSp$index]
+sIndSp<-dplyr::select(subset(countSP_ISA$sign, p.value<0.05),index,stat,p.value)
+sIndSp$Species<-rownames(sIndSp)
+sIndSp$time<-colnames(countSP_ISA$B)[sIndSp$index]
 
-viridis::viridis(n=3,end=0.95,option="C")
+
 #Need new axes R2 scores (from PC-ORD)
 #Probably want to do the years on a plot and then a split 2017-2018 two panel plot (so 3 total)
 ggplot(data=NMDS_scores,aes(MDS1,MDS2))+
